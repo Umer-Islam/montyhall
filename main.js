3e0c41bc323091122ffd0a1558a1365d0388c5a0
Umer-Islam <139012668+Umer-Islam@users.noreply.github.com>
2024-05-17 06:13:46 +0500

    make DOM functions

---
+++ main.js
@@ -0,0 +1,30 @@
+door_1 = document.querySelector('.door1').addEventListener('click',funcDoor1)
+door_2 = document.querySelector('.door2').addEventListener('click',funcDoor2)
+door_3 = document.querySelector('.door3').addEventListener('click',funcDoor3)
+
+
+
+let car = Math.random()*3 
+
+
+if 
+
+
+
+
+
+
+
+function funcDoor1(){
+    document.querySelector('.door1').innerText = "🐐"
+}
+
+function funcDoor2(){
+    
+    document.querySelector('.door2').innerText = "❌"
+    document.querySelector('h2').innerText = "Think again"
+}
+
+function funcDoor3(){}
+
+
